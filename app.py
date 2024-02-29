79092c08d199ed9207208ec08b5afbe788caee6a
vinayakbymsli <161317272+vinayakb-ymsli@users.noreply.github.com>
2024-02-29 12:51:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
+#HI
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
